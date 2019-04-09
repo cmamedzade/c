023fbfb5e57fcfa342624028acf16e5fae52c9c2
cmamedzade <41629353+cmamedzade@users.noreply.github.com>
2019-04-09 16:50:43 +0400

    Add files via upload

---
+++ convertIntToHexString/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"reverse.h"
+#define TRUE 0
+#define FALSE 1
+
+enum hex {A = 10,B = 11,C = 12, D = 13, E  = 14, F = 15};
+
+char a[];
+
+int main()
+{
+    itoh(-30);
+    reverse(a);
+}
+
+
+int itoh(int n)
+
+{
+    int f,s,bool,i = 0;
+    s = n;
+    char sign  = '-';
+
+    if (s < 0)
+    {
+        s = s * -1;
+        bool = TRUE;
+    }
+
+        while (s > 16)
+        {
+            f = s % 16;
+            s = s / 16;
+            if (f >= A && f <= F)
+            {
+                a[i++] = f + 55;
+            }
+            else
+            {
+                a[i++] = f + '0';
+            }
+
+        }
+        a[i++] = s + '0';
+        if (bool == TRUE)
+            a[i] = '-';
+        a[++i] = '\0';
+}
+
+
+
